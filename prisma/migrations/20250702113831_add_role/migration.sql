d9864e45ad00ecdc1de599915570da29ae68719e
gab-cat <catimbanggabriel@gmail.com>
2025-07-04 15:48:31 +0800

    Initialize Prisma schema with SQLite datasource and update User model to use roles as a string. Remove previous PostgreSQL migrations and migration lock file.

--- prisma/migrations/20250702113831_add_role/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateEnum
-CREATE TYPE "Role" AS ENUM ('USER', 'STAFF', 'ADMIN');
-
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'USER';
-
--- CreateIndex
-CREATE INDEX "users_email_idx" ON "users"("email");
